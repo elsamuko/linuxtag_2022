e6ee9f6993a4be4a5af552fdf1863263f3f68a7d
Samuel Albrecht <elsamuko@gmail.com>
2022-05-30 18:29:21 +0200

    add: print version

--- linuxtag/linuxtag/__main__.py
+++ linuxtag/linuxtag/__main__.py
@@ -1,6 +1,17 @@
 
+import importlib.metadata as metadata
+
+
+def get_version() -> str:
+    try:
+        return str(metadata.version("linuxtag"))
+    except metadata.PackageNotFoundError:
+        return "0.0"
+
+
 def main() -> None:
     print("Hello!")
+    print(get_version())
 
 
 if __name__ == "__main__":
