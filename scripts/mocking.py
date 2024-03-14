d73a129358d26e00d66861a4af2357a21b862639
Samuel Albrecht <elsamuko@gmail.com>
2024-03-14 17:12:56 +0100

    add: usages for Mock, MagickMock

--- scripts/mocking.py
+++ scripts/mocking.py
@@ -2,16 +2,32 @@
 # python3 -m unittest scripts.mocking
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock, Mock
 
 
 def original():
     return "original"
 
 
+def func(arg):
+    print(arg.sth())
+    try:
+        parsed = int(arg)
+        print(f"Parsed {arg} to {parsed}")
+    except:
+        print(f"Could not parse {arg} to int")
+
+
 class Test(unittest.TestCase):
     @patch(f"{__name__}.original")
-    def test_func(self, mock):
+    def test_original(self, mock):
         print(f"patching {__name__}.original")
         mock.return_value = "replacement"
         print(original())
+
+    def test_func(self):
+        print(f"Mocking arg")
+        func(MagicMock())
+        print()
+        func(Mock())
+        print()
